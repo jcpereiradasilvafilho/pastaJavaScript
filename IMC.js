b83272b937f8ffaf954ad4b0eaecc517e4e600ab
José Carlos Pereira <jc20e5@gmail.com>
2024-05-24 11:10:46 -0300

    melhorado o programa IMC

--- IMC.js
+++ IMC.js
@@ -7,9 +7,23 @@ altura = parseFloat(prompt("Qual sua altura: "));
 peso = parseFloat(prompt("Qual seu peso: "));
 
 function calculaIMC(altura,peso){
-    IMC = peso/(altura*altura);
+    IMC = (peso/(altura*altura)).toFixed(1);
     return IMC;
 }
 calculaIMC(altura,peso);
 
-console.log("O valor do seu IMC é "+IMC+".");
+if (IMC < 18.5) {
+    console.log(`O valor do seu IMC é ${IMC}. Você está abaixo do peso.`);
+} else if (IMC >= 18.5 && IMC <= 24.9) {
+    console.log(`O valor do seu IMC é ${IMC}. Você está com o peso normal.`);
+} else if (IMC >= 25 && IMC <= 29.9) {
+    console.log(`O valor do seu IMC é ${IMC}. Você está com sobrepeso.`);
+} else if (IMC >= 30 && IMC <= 34.9) {
+    console.log(`O valor do seu IMC é ${IMC}. Você está com obesidade grau I.`);
+} else if (IMC >= 35 && IMC <= 39.9) {
+    console.log(`O valor do seu IMC é ${IMC}. Você está com obesidade grau II.`);
+} else if (IMC >= 40) {
+    console.log(`O valor do seu IMC é ${IMC}. Você está com obesidade grau III.`);
+} else {
+    console.log("Valor inválido.");
+}
